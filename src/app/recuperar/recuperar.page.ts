13c281669c3bd8d4f67099061e1fbbfab8fce7ca
HalconNocturno <167113946+HalconNocturno@users.noreply.github.com>
2024-09-12 20:57:38 -0300

    Recuperar

--- src/app/recuperar/recuperar.page.ts
+++ src/app/recuperar/recuperar.page.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-recuperar',
   templateUrl: './recuperar.page.html',
   styleUrls: ['./recuperar.page.scss'],
 })
-export class RecuperarPage implements OnInit {
+export class RecuperarPage {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+
+  goHome() {
+    this.router.navigate(['/home']);
   }
 
 }
